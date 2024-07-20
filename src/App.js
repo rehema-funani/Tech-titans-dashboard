1d2d1ec6264da2527c2d64dfd7c83d65a5745e17
rehema_funani <rehemafunani@gmail.com>
2024-07-20 08:44:16 +0300

    adds login page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ImageDisplay from './ImageDisplay';
+
+function App() {
+  return (
+    <div className="App">
+      <ImageDisplay/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ImageDisplay/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './LoginScreen.css';
+const LoginScreen = () => {
+  return (
+    <div className="login-container">
+      <div className="left-panel">
+        <div className="logo-container">
+          {/* Add your logo image here */}
+          <img src="/image/logo.png" alt="Logo" className="logo" />
+        </div>
+        <h1 className="dashboard-title">UPCYCLEIT DASHBOARD</h1>
+      </div>
+      <div className="right-panel">
+        <h2 className="login-title">Log In</h2>
+        <form className="login-form">
+          <input type="text" placeholder="FullName" className="input-field" />
+          <input type="text" placeholder="Member Id" className="input-field" />
+          <input type="password" placeholder="Password" className="input-field" />
+          <button type="submit" className="login-button">Log In</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default LoginScreen;
