020f9561a3de09d66532cb58445372c5a05a0d27
rehema_funani <rehemafunani@gmail.com>
2024-07-20 10:31:20 +0300

    adds login

--- src/ImageDisplay/index.js
+++ src/ImageDisplay/index.js
@@ -12,8 +12,11 @@ const LoginScreen = () => {
       <div className="right-panel">
         <h2 className="login-title">Log In</h2>
         <form className="login-form">
+          <label>Full name</label><br></br>
           <input type="text" placeholder="FullName" className="input-field" />
+          <label>MemberId</label>
           <input type="text" placeholder="Member Id" className="input-field" />
+          <label>Password</label>
           <input type="password" placeholder="Password" className="input-field" />
           <button type="submit" className="login-button">Log In</button>
         </form>
